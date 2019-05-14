3e6bf53d54a21b6f7821d29825b923bd1f1b9774
maxime.zegres <maxime.zegres@gmail.com>
2019-05-14 23:09:41 +0200

    Add method for Event and Game to repository

--- EsportEventBelgium/Services/EsportEventRepository.cs
+++ EsportEventBelgium/Services/EsportEventRepository.cs
@@ -15,6 +15,7 @@ namespace EsportEventBelgium.Services
             _context = context;
         }
 
+
         public IEnumerable<Organization> GetOrganizations()
         {
             return _context.Organizations.OrderBy(o => o.Name);
@@ -35,6 +36,51 @@ namespace EsportEventBelgium.Services
             _context.Organizations.Remove(organization);
         }
 
+
+
+
+        public IEnumerable<Event> GetEvents()
+        {
+            return _context.Events.OrderBy(o => o.BeginDate);
+        }
+
+        public Event GetEvent(int id)
+        {
+            return _context.Events.FirstOrDefault(o => o.Id == id);
+        }
+
+        public void AddEvent(Event esportEvent)
+        {
+            _context.Events.Add(esportEvent);
+        }
+
+        public void DeleteEvent(Event esportEvent)
+        {
+            _context.Events.Remove(esportEvent);
+        }
+
+
+
+
+        public IEnumerable<Game> GetGames()
+        {
+            return _context.Games.OrderBy(g => g.Name);
+        }
+
+        public Game GetGame(int id)
+        {
+            return _context.Games.FirstOrDefault(o => o.Id == id);
+        }
+
+        public void AddGame(Game game)
+        {
+            _context.Games.Add(game);
+        }
+
+        public void DeleteGame(Game game)
+        {
+            _context.Games.Remove(game);
+        }
 
 
     }
--- EsportEventBelgium/Services/IEsportEventRepository.cs
+++ EsportEventBelgium/Services/IEsportEventRepository.cs
@@ -8,10 +8,23 @@ namespace EsportEventBelgium.Services
 {
     public interface IEsportEventRepository
     {
+        // Organization
         IEnumerable<Organization> GetOrganizations();
         Organization GetOrganization(int id);
         void AddOrganization(Organization organization);
         void DeleteOrganization(Organization organization);
 
+
+        // Event
+        IEnumerable<Event> GetEvents();
+        Event GetEvent(int id);
+        void AddEvent(Event esportEvent);
+        void DeleteEvent(Event esportEvent);
+
+        // Game
+        IEnumerable<Game> GetGames();
+        Game GetGame(int id);
+        void AddGame(Game game);
+        void DeleteGame(Game game);
     }
 }
